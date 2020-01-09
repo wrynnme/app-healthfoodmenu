caca344a25689491de277b22c430e065241a1800
wrynnme <57111028+wrynnme@users.noreply.github.com>
2020-01-10 01:27:17 +0700

    Update userscontr.class.php

--- classes/userscontr.class.php
+++ classes/userscontr.class.php
@@ -2,6 +2,76 @@
 
 class userscontr extends users {
 
+	public function reset_password($email) {
+		$stmt = $this->SELECT_EMAIL($email);
+		$row = $stmt->rowCount();
+		if ((int)$row != 1) {
+			echo 'error mail not match';
+			exit();
+		}
+		$selector = bin2hex(random_bytes(32));
+		$token = random_bytes(8);
+		$url = "reset_newpassword.php?s=".$selector."&t=".bin2hex($token);
+		$expires = date("U") + 1800;
+
+		$del = $this->DELETE_PWD($email);
+		
+		if (!$del) {
+			echo 'error delete pwd_reset';
+			exit();
+		} else {
+			$del = true;
+		}
+		$hashedToken = password_hash($token, PASSWORD_DEFAULT);
+		$ins = $this->INSERT_PWD($email, $selector, $hashedToken, $expires);
+
+		if (!$ins) {
+			echo 'error delete pwd_reset';
+			exit();
+		} else {
+			$ins = true;
+			return $url;
+		}
+	}
+
+	public function reset_newpassword($selector, $validator, $currentDate, $pwd) {
+
+		$stmt = $this->SELECT_PWD($selector, $currentDate);
+		if (!$row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+			echo 'You need to re-submit your reset request.';
+			exit();
+		} else {
+			$tokenBinary = hex2bin($validator);
+			$tokenCheck = password_verify($tokenBinary, $row['pwd_token']);
+			if ($tokenCheck === false) {
+				echo 'You need to re-submit your reset request.';
+				exit();
+			} elseif ($tokenCheck === true) {
+				$tokenEmail  = $row['pwd_email'];
+				$stmt_mail = $this->SELECT_EMAIL($tokenEmail);
+				if (!$row = $stmt_mail->fetch(PDO::FETCH_ASSOC)) {
+					echo 'There was an error!';
+					exit();
+				} else {
+					$newpwd = password_hash($pwd, PASSWORD_DEFAULT); 
+					$update = $this->UPDATE_EMAIL('cus_pass', $newpwd, $tokenEmail);
+
+					if (!$update) {
+						echo 'Can\'t update new password!';
+						exit();
+					} else {
+						$del = $this->DELETE_PWD($tokenEmail);
+						if ($del) {
+							echo 'password has change';
+							exit();
+						}
+					}
+				}
+			}
+		}
+		
+	}
+
 	public function create($fname, $lname, $rname, $email, $pass, $tel) {
 		$this->INSERT($fname, $lname, $rname, $email, $pass, $tel);
 	}
