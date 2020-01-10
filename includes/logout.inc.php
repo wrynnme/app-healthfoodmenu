5cfe81333f60324941102f39d8087b40db0ae1f0
wrynnme <57111028+wrynnme@users.noreply.github.com>
2020-01-10 23:08:06 +0700

    Update logout.inc.php

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,6 +1,6 @@
 <?php require_once 'class-autoload.inc.php'; ?>
 <?php
-$logout = new UsersContr();
+$logout = new userscontr();
 $logout->change('cus_login', '0', $_SESSION['cus_id']);
 unset($logout);
 session_destroy();
