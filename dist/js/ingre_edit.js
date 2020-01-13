632d76eaa5850b498fc1da4768b3855dbe3974f8
wrynnme <57111028+wrynnme@users.noreply.github.com>
2020-01-14 02:10:05 +0700

    edit bug

--- dist/js/ingre_edit.js
+++ dist/js/ingre_edit.js
@@ -2,7 +2,6 @@ $('.custom-file-input').change(function(event) {
 	$('.custom-file-label').text($(this).val().split("\\").pop());
 });
 'use strict';
-
 window.addEventListener('load', function() {
 	var forms = $('.needs-validation');
 	var validation = Array.prototype.filter.call(forms, function(form) {
@@ -14,26 +13,29 @@ window.addEventListener('load', function() {
 				event.preventDefault();
 				var myForm = document.getElementById('myForm');
 				var data = new FormData(myForm);
+				var id = <?php echo $_GET['id']; ?>;
+				var old_pic = "<?php echo $ing['ing_img']; ?>";
+				data.append('ing_id', id);
+				data.append('old_pic', old_pic);
 				// for(var pair of data.entries()) {
 				// 	console.log(pair[0]+ ', '+ pair[1]); 
 				// }
 				$.ajax({
-					url: 'includes/ingredients_add.inc.php',
+					url: 'includes/ingredients_edit.inc.php',
 					type: 'POST',
 					data: data,
 					processData: false,
 					contentType: false,
 					success: function(response) {
-						if (response == '1') {
-							Swal.fire("สำเร็จ !", "<b>รอการตรวจสอบจากผู้ดูแล !!</b>", "success").then(function(){
-								window.location.href='index.php';
+						if (response == '') {
+							Swal.fire("สำเร็จ !", "<b>แก้ไขข้อมูลสำเร็จ !!</b>", "success").then(function(){
+								window.location.href='ingredients_list.php';
 							})
 						}else{
 							Swal.fire("ไม่สำเร็จ !", "<b>กรุณาตรวจสอบข้อมูลที่กรอก !!</b>", "error")
 						}
 					}
-
-				});
+				})
 			}
 			form.classList.add('was-validated');
 		}, false);
--- ingredients_edit.php
+++ ingredients_edit.php
@@ -93,52 +93,6 @@ $type = $ings->type();
 		</form>
 	</div>
 	<script src="dist/js/be.js"></script>
-	<script>
-		$(document).ready(function() {
-			$('.custom-file-input').change(function(event) {
-				$('.custom-file-label').text($(this).val().split("\\").pop());
-			});
-			'use strict';
-			window.addEventListener('load', function() {
-				var forms = $('.needs-validation');
-				var validation = Array.prototype.filter.call(forms, function(form) {
-					form.addEventListener('submit', function(event) {
-						if (form.checkValidity() === false) {
-							event.preventDefault();
-							event.stopPropagation();
-						}else{
-							event.preventDefault();
-							var myForm = document.getElementById('myForm');
-							var data = new FormData(myForm);
-							var id = <?php echo $_GET['id']; ?>;
-							var old_pic = "<?php echo $ing['ing_img']; ?>";
-							data.append('ing_id', id);
-							data.append('old_pic', old_pic);
-							/*for(var pair of data.entries()) {
-								console.log(pair[0]+ ', '+ pair[1]); 
-							}*/
-							$.ajax({
-								url: 'includes/ingredients_edit.inc.php',
-								type: 'POST',
-								data: data,
-								processData: false,
-								contentType: false,
-								success: function(response) {
-									if (response == '') {
-										Swal.fire("สำเร็จ !", "<b>แก้ไขข้อมูลสำเร็จ !!</b>", "success").then(function(){
-											window.location.href='ingredients_list.php';
-										})
-									}else{
-										Swal.fire("ไม่สำเร็จ !", "<b>กรุณาตรวจสอบข้อมูลที่กรอก !!</b>", "error")
-									}
-								}
-							})
-						}
-						form.classList.add('was-validated');
-					}, false);
-				});
-			}, false);
-		});
-	</script>
+	<script src="dist/js/ingre_edit.js"></script>
 </body>
 </html>
